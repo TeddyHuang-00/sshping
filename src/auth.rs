171ed3e08fad7f9487d525f5227baea464cae384
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2024-06-12 17:59:33 +0800

    feat: Time ssh authentication

--- src/auth.rs
+++ src/auth.rs
@@ -1,5 +1,6 @@
 use log::{info, warn};
 use std::path::PathBuf;
+use std::time::{Duration, Instant};
 
 use ssh2::Session;
 
@@ -8,7 +9,7 @@ pub fn authenticate_all(
     user: &str,
     password: Option<&str>,
     identity: Option<&PathBuf>,
-) -> Result<(), String> {
+) -> Result<Duration, String> {
     let methods = session
         .auth_methods(user)
         .unwrap()
@@ -16,41 +17,38 @@ pub fn authenticate_all(
         .collect::<Vec<&str>>();
     info!("Available authentication methods: {methods:?}");
     // Try all authentication methods in order of preference
+    let now = Instant::now();
     match session.userauth_agent(user) {
         Ok(_) => {
             info!("Agent authentication succeeded");
-            return Ok(());
+            return Ok(now.elapsed());
         }
-        Err(e) => {
-            warn!("Agent authentication failed: {e}");
-        }
+        Err(e) => warn!("Agent authentication failed: {e}"),
     }
     if let Some(identity) = identity {
         if !methods.contains(&"publickey") {
             warn!("Public key authentication not supported on server");
         } else {
+            let now = Instant::now();
             match session.userauth_pubkey_file(user, None, identity, password) {
                 Ok(_) => {
                     info!("Public key authentication succeeded");
-                    return Ok(());
+                    return Ok(now.elapsed());
                 }
-                Err(e) => {
-                    warn!("Pubkey authentication failed: {e}");
-                }
+                Err(e) => warn!("Pubkey authentication failed: {e}"),
             }
         }
     }
     if !methods.contains(&"password") {
         warn!("Password authentication not supported on server");
     } else {
+        let now = Instant::now();
         match session.userauth_password(user, password.unwrap_or_default()) {
             Ok(_) => {
                 info!("Password authentication succeeded");
-                return Ok(());
+                return Ok(now.elapsed());
             }
-            Err(e) => {
-                warn!("Password authentication failed: {e}");
-            }
+            Err(e) => warn!("Password authentication failed: {e}"),
         }
     }
     // Fails if all authentication methods fail
